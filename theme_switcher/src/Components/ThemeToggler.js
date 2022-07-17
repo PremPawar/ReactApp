5da6e83b78e1d718de97ce2887b1a3aa213e4dd7
Prem <prempawar28@gmail.com>
2022-07-17 20:38:22 +0530

    Theme Switcher
    Theme Switcher using react context.

---
+++ theme_switcher/src/Components/ThemeToggler.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import ThemeContext from '../Context/ThemeContext';
+
+const ThemeToggler = () => {
+    const [themeMode, setThemeMode] = useContext(ThemeContext);
+
+    return (
+        <button onClick={() => {
+            setThemeMode(themeMode === "light" ? "dark" : "light");
+        }}>
+            <span>{themeMode === "light" ? "Turn Off" : "Lights On"}</span>
+        </button>
+    );
+};
+
+export default ThemeToggler;
